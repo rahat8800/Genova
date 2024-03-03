b0bedc9557e44a2690f8006cdbacd3c7ce8af942
Rahat <rahat.pasha@azularc.com>
2024-03-03 13:52:29 +0530

    added gitignore and removed pods

--- Pods/Target Support Files/Pods-Genova-GenovaUITests/Pods-Genova-GenovaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Genova_GenovaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Genova_GenovaUITests
-@end
--- Pods/Target Support Files/Pods-GenovaTests/Pods-GenovaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GenovaTests : NSObject
-@end
-@implementation PodsDummy_Pods_GenovaTests
-@end
